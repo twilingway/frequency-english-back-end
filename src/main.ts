c917ad0fe0c76bb5725a844fdb74cb0e7327c6cb
Aleksei Glazunov <993468@gmail.com>
2023-09-17 22:30:09 +0400

    add webhook

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { getBotToken } from 'nestjs-telegraf';
+import { getBotToken } from 'nestjs-telegraf';
 // import { config } from 'dotenv';
 // config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const bot = app.get(getBotToken());
+  app.use(bot.webhookCallback('/bot'));
   app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(3030);
--- src/telegram/telegram-config.factory.ts
+++ src/telegram/telegram-config.factory.ts
@@ -3,7 +3,7 @@ import {
   TelegrafModuleAsyncOptions,
   TelegrafModuleOptions,
 } from 'nestjs-telegraf';
-import LocalSession from 'telegraf-session-local';
+// import LocalSession from 'telegraf-session-local';
 
 // const sessions = new LocalSession({ database: 'session_db.json' });
 
@@ -13,6 +13,12 @@ const telegrafModuleOptions = (
   return {
     token: configService.get('TELEGRAM_API_TOKEN'),
     // middlewares: [sessions.middleware()],
+    launchOptions: {
+      webhook: {
+        domain: 'https://frequency-english-bot.twiling.ru',
+        hookPath: '/bot',
+      },
+    },
   };
 };
 
